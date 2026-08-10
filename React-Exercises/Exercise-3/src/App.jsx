ee285564747d5c704006fa662c9469ed1806327d
proffmohameddev <proffmohamedtahliil@gmail.com>
2026-08-10 23:44:40 +0300

    ex3

---
+++ React-Exercises/Exercise-3/src/App.jsx
@@ -0,0 +1,13 @@
+import UseCard from "./UseCard";
+
+function app() {
+  return (
+    <>
+      <UseCard Uname={"Proff"} email={"proff@gmail.com"} />
+      <UseCard Uname={"Moha"} email={"moha@gmail.com"} />
+      <UseCard Uname={"Mc"} email={"Mc@gmail.com"} />
+    </>
+  );
+}
+
+export default app;
---
+++ React-Exercises/Exercise-3/src/UseCard.jsx
@@ -0,0 +1,12 @@
+const UseCard = ({ Uname, email }) => {
+  return (
+    <>
+      <div>
+        <h1>Welcome: {Uname}</h1>
+        <p>Your Email : {email}</p>
+      </div>
+    </>
+  );
+};
+
+export default UseCard;
