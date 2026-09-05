0940842626633c4f9d524cc8da2e16a622d5cd22
proffmohameddev <proffmohamedtahliil@gmail.com>
2026-09-05 07:26:06 +0300

    ex13

---
+++ React-Exercises/Exercise #13/src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from "react";
+
+import DoubleCounter from "./DoubleCounter";
+
+function App() {
+  return (
+    <>
+      <DoubleCounter />
+      <doubleCounterReducer />
+    </>
+  );
+}
+
+export default App;
