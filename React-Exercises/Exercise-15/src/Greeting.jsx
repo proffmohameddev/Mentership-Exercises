56d5041380a900411ffed6eaef96fdb1aaed7a01
proffmohameddev <proffmohamedtahliil@gmail.com>
2026-09-06 08:30:16 +0300

    ex15-16

---
+++ React-Exercises/Exercise-15/src/Greeting.jsx
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import LanguageContext from "./LanguageContext";
+
+function Greeting() {
+  const language = useContext(LanguageContext);
+
+  const messages = {
+    en: "Hello!",
+    es: "¡Hola!",
+  };
+
+  return <h1>{messages[language]}</h1>;
+}
+
+export default Greeting;
---
+++ React-Exercises/Exercise-15/src/LanguageContext.js
@@ -0,0 +1,3 @@
+import { createContext } from "react";
+const LanguageContext = createContext("en"); // Default is English
+export default LanguageContext;
