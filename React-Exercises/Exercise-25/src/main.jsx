90d5448c5508da1ba612d9419b1fbc3373555cee
proffmohameddev <proffmohamedtahliil@gmail.com>
2026-09-17 08:33:59 +0300

    ex25

---
+++ React-Exercises/Exercise-25/src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { router } from "./router";
+import { RouterProvider } from "react-router";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { RouterProvider } from "react-router-dom";
+// import { router } from "./routes";
+// import "./index.css";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>,
+// );
